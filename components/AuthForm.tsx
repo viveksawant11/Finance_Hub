fdc994311247691ceecd68a085c65c8ad4b7eea2
viveksawant11 <viveksawant011@gmail.com>
2025-01-24 08:47:54 +0530

    Implement CustomInput.tsx for signin form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -4,8 +4,42 @@ import Link from "next/link";
 import Image from "next/image";
 import React, { useState } from "react";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import CustomInput from "./CustomInput";
+import { authFormSchema } from "@/lib/utils";
+
 const AuthForm = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof authFormSchema>>({
+    resolver: zodResolver(authFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof authFormSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
 
   return (
     <section className="auth-form">
@@ -34,6 +68,32 @@ const AuthForm = ({ type }: { type: string }) => {
           </h1>
         </div>
       </header>
+
+      {user ? (
+        <div className="flex">{/* {PlaidLink} */}</div>
+      ) : (
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <CustomInput
+                control={form.control}
+                name="email"
+                label="Email"
+                placeholder="Enter your email"
+              />
+
+              <CustomInput
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Enter your password"
+              />
+
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </>
+      )}
     </section>
   );
 };
