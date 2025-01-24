a29014a828168712e06f1ae95831471f5787bdd9
viveksawant11 <viveksawant011@gmail.com>
2025-01-24 07:32:26 +0530

    added usesr in authform

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -26,6 +26,11 @@ const AuthForm = ({ type }: { type: string }) => {
         <div className="flex flex-col gap-1 md:gap-3">
           <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
             {user ? "Link Account" : type === "sign-in" ? "Sign-In" : "Sign Up"}
+            <p className="text-16 font-normal text-gray-600">
+              {user
+                ? "Link your account to get started"
+                : "Please enter your details"}
+            </p>
           </h1>
         </div>
       </header>
